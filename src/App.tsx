115b528a50bf53f42126fb59dd4183e25dcbd6e0
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-08-13 11:29:14 -0500

    start over

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import { NavBar } from "./components/nav/NavBar";
-import { HomePage } from "./pages/home/HomePage";
-import { AboutPage } from "./pages/about/AboutPage";
-import { ContactPage } from "./pages/contact/ContactPage";
-
-export function App() {
+function App() {
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route path="/" >
-            <HomePage />
-          </Route>
-          <Route path="/about">
-            <AboutPage />
-          </Route>
-          <Route path="/contact">
-            <ContactPage />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
