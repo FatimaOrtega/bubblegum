def7dc2124caeef23a7587a78b70c7ccca0995ed
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-08-15 11:19:40 -0500

    Added a footer and main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,46 @@
-import React, { Component } from 'react';
-import 'reset-css'
-import Navbar from './components/navbar/Navbar';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import About from './pages/about/About';
-import Yasmin from './pages/yasmin/Yasmin';
-import Contact from './pages/contact/Contact';
-
+import React, { Component } from "react";
+import "reset-css";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/about/About";
+import Yasmin from "./pages/yasmin/Yasmin";
+import Contact from "./pages/contact/Contact";
+import Main from "./pages/Main/Main";
+import { Footer } from "./components/footer/Footer";
 
 const navigation = {
   brand: { name: "Bubblegumgemz", to: "" },
   links: [
-    { name: "Home", to: '/'},
-    { name: "Shop", to: '/'},
+    { name: "Home", to: "/" },
+    { name: "Shop", to: "/" },
     { name: "Yasmin", to: "/yasmin" },
     { name: "About Us", to: "/about" },
     { name: "Contact Information", to: "/contact" },
-    
-  ]
-}
+  ],
+};
 
 export default class App extends Component {
-  // the 'public' is a typescript feature. 
+  // the 'public' is a typescript feature.
   public render() {
-
-	// Descructured object for cleaner code :-)
+    // Descructured object for cleaner code :-)
     const { brand, links } = navigation;
 
     return (
       <div className="App">
         <Navbar brand={brand} links={links} />
-        
-         {/* Takes links to routed pages */}
-        <Router>
-        <Routes>       
-        <Route 
-            path="/about" 
-            element={<About/>}
-        />
 
-        <Route 
-            path="/yasmin"
-            element={<Yasmin />}
-        />
-        <Route 
-            path="/contact"
-            element={<Contact />}
-        />
-        </Routes>
-      </Router>
+        {/* Takes links to routed pages */}
+        <Router>
+          <Routes>
+            <Route path="/" element={<Main />} />
+
+            <Route path="/about" element={<About />} />
+
+            <Route path="/yasmin" element={<Yasmin />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+        <Footer />
       </div>
     );
   }
