7b37d113ad9e907dc49c9b16a662852c5cdd8153
FatimaOrtega <75735876+FatimaOrtega@users.noreply.github.com>
2022-08-15 08:53:38 -0500

    added navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import data from "./components/content/main.json";
+import { Card } from './components/sections/cards/Card';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
   <div>
-  hello
+  {data.eventCategories.map((event, index) => {
+          if (index < 3) {
+            return (
+              <Card
+                image={event.image}
+                title={event.title}
+                description={event.description}
+                date={event.date}
+                location={event.location}
+                type={event.type}
+              ></Card>
+            );
+          }
+        })}
   </div>
   );
 }
